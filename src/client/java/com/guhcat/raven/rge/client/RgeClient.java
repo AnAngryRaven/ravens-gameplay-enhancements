535403f9dd9f16632f8b8523e79e60fd5dfe979f
anangryraven <39865912+AnAngryRaven@users.noreply.github.com>
2025-03-17 12:34:10 -0400

    Init

---
+++ src/client/java/com/guhcat/raven/rge/client/RgeClient.java
@@ -0,0 +1,10 @@
+package com.guhcat.raven.rge.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RgeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/guhcat/raven/rge/Rge.java
@@ -0,0 +1,10 @@
+package com.guhcat.raven.rge;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Rge implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
