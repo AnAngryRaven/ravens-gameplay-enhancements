21722fe7d952768572120ae06527fb85c1b8cf28
anangryraven <39865912+AnAngryRaven@users.noreply.github.com>
2025-04-05 00:11:54 -0400

    Enchanting table influencing pt II Still needs some balancing / might not work correctly, but rn i'm getting it checked into version control at least

--- src/main/java/com/guhcat/raven/rge/mixin/EnchantingTableInfluencing.java
+++ src/main/java/com/guhcat/raven/rge/mixin/EnchantingTableInfluencing.java
@@ -1,32 +1,47 @@
 package com.guhcat.raven.rge.mixin;
 
 import com.guhcat.raven.rge.EnchantmentInfluenceHashmap;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.ChiseledBookshelfBlockEntity;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.screen.EnchantmentScreenHandler;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @Mixin(EnchantmentScreenHandler.class)
 public abstract class EnchantingTableInfluencing extends ScreenHandler implements EnchantmentInfluenceHashmap {
+
+    @Shadow @Final private Random random;
+
+    @Shadow @Final public int[] enchantmentPower;
+
+    int iter = 0;
 
     protected EnchantingTableInfluencing(@Nullable ScreenHandlerType<?> type, int syncId) {
         super(type, syncId);
@@ -53,20 +68,52 @@ public abstract class EnchantingTableInfluencing extends ScreenHandler implement
                     continue;
 
                 for (RegistryEntry<Enchantment> e : bookStack.getComponents().get(DataComponentTypes.STORED_ENCHANTMENTS).getEnchantments()) {
-                    enchantmentInfluence.put(e, enchantmentInfluence.getOrDefault(e, 0) + 1);
+
+                    //todo!(); Change weighting for this. Seems the weights influence things a little too easily
+                    // - Or maybe the implementation for helperFunction is effed, idk. that's a later me problem tho
+                    int influenceNumber = Math.min(enchantmentInfluence.getOrDefault(e, 0) + bookStack.getComponents().get(DataComponentTypes.STORED_ENCHANTMENTS).getLevel(e), 20);
+                    enchantmentInfluence.put(e, influenceNumber);
                 }
             }
         }
         ix.set(ix.get() + (int) chiselPower);
-
-        System.out.println(enchantmentInfluence);
     }
 
 
-    @Inject(method = "method_17411", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/EnchantmentScreenHandler;generateEnchantments(Lnet/minecraft/registry/DynamicRegistryManager;Lnet/minecraft/item/ItemStack;II)Ljava/util/List;", shift = At.Shift.BY, by = 2))
-    public void influenceEnchants(ItemStack itemStack, World world, BlockPos pos, CallbackInfo ci, @Local List<EnchantmentLevelEntry> list){
-        if(!enchantmentInfluence.isEmpty()){
-            //todo!(); Actually influence enchantments
-        }
+    @WrapOperation(method = "method_17411", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/EnchantmentScreenHandler;generateEnchantments(Lnet/minecraft/registry/DynamicRegistryManager;Lnet/minecraft/item/ItemStack;II)Ljava/util/List;"))
+    public List<EnchantmentLevelEntry> influenceEnchants(EnchantmentScreenHandler instance, DynamicRegistryManager registryManager, ItemStack stack, int slot, int level, Operation<List<EnchantmentLevelEntry>> original){
+        return helperFunction(instance, registryManager, stack, slot, level, original);
+    }
+
+    @WrapOperation(method = "method_17410", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/EnchantmentScreenHandler;generateEnchantments(Lnet/minecraft/registry/DynamicRegistryManager;Lnet/minecraft/item/ItemStack;II)Ljava/util/List;"))
+    public List<EnchantmentLevelEntry> influenceApplied(EnchantmentScreenHandler instance, DynamicRegistryManager registryManager, ItemStack stack, int slot, int level, Operation<List<EnchantmentLevelEntry>> original){
+        return helperFunction(instance, registryManager, stack, slot, level, original);
+    }
+
+    /**
+     * If there are enchantments to influence, then generate a random list and add the influenced enchantments
+     *
+     * Note that this function only really exists to prevent duplicate code; For whatever reason, the
+     * vanilla implementation appears to have duplicated code for generating the enchantments and then
+     * actually applying them.
+     * @param instance Instance of EnchantmentScreenHandler
+     * @param registryManager Registry manager of the world
+     * @param stack Item to be enchanted
+     * @param slot Enchanting table enchantment slot
+     * @param level Level of the enchantment
+     * @param original Original operation
+     * @return
+     */
+    @Unique
+    public List<EnchantmentLevelEntry> helperFunction(EnchantmentScreenHandler instance, DynamicRegistryManager registryManager, ItemStack stack, int slot, int level, Operation<List<EnchantmentLevelEntry>> original) {
+        if(enchantmentInfluence.isEmpty())
+            return original.call(instance, registryManager, stack, slot, level);
+
+        ArrayList<RegistryEntry<Enchantment>> tempList = new ArrayList<>(enchantmentInfluence.keySet().stream().toList());
+        original.call(instance, registryManager, stack, slot, level).forEach(e -> {
+            tempList.add(e.enchantment());
+        });
+
+        return EnchantmentHelper.generateEnchantments(random, stack, level, tempList.stream());
     }
 }
